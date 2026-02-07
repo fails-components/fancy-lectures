4675227e92b547c75e2d0620c20edfa2aaacdbac
Marten Richter <marten.richter@tu-berlin.de>
2026-02-07 12:09:59 +0100

    Fix build

--- packages/lectureapp/src/socket/interface.js
+++ packages/lectureapp/src/socket/interface.js
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-import { KeyStore } from '../misc/keystore'
+import { KeyStore } from '../misc/keystore.js'
 export class SocketInterface {
   static worker = new Worker(new URL('./worker.js', import.meta.url), {
     type: 'module'
