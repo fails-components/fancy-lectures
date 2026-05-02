6db006c0ee3895e684cb90fb441a13dbd913617a
Marten Richter <marten.richter@tu-berlin.de>
2026-05-02 09:09:05 +0200

    Fix bug in proxy config

--- daemons/staticserver/40-copy-fails-env.sh
+++ daemons/staticserver/40-copy-fails-env.sh
@@ -4,7 +4,7 @@ if [[ -n "${FAILS_APP_CONFIG_JSON}" ]]; then
   echo ${FAILS_APP_CONFIG_JSON} > /usr/share/nginx/html/config/app.json
   chmod gou+r /usr/share/nginx/html/config/app.json
 fi
-if [[ -n "${FAILS_APP_CONFIG_JSON}" ]]; then
+if [[ -n "${FAILS_JUPYTER_PROXY_CONFIG}" ]]; then
   echo ${FAILS_JUPYTER_PROXY_CONFIG} > /usr/share/nginx/html/config/proxy.json
   chmod gou+r /usr/share/nginx/html/config/proxy.json
 fi
